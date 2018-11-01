9246077974e251d4b8ed19e07f4ed58b10c2d020
hybridbad <hybridbad90@gmail.com>
2018-11-01 10:44:53 +0000

    update

--- katas/sum_of_numbers.rb
+++ katas/sum_of_numbers.rb
@@ -3,3 +3,11 @@ def get_sum(a,b)
   sum_arr = arr[0]..arr[-1].to_a
   sum_arr.each { |n| sum+=n }
 end
+
+
+#better one with sum method from newer ruby
+def get_sum(a,b)
+  arr = [a,b].sort
+  (arr.first..arr.last).each.sum
+end
+
